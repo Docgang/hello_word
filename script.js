b5537082f603c5c3ef79671f05a44be69c7c9128
moses <darkelfdumo@gmail.com>
2022-05-23 21:28:04 +0300

    practiced on my git skills

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let a = 10;
-alert(a);
+let text = "HELLO WORLD";
+let char = text[8];
+console.log(char);
+let color_red = "#rrr";
+let color_blue = "#bbb";
+let color_brown = "#fff";
+let first_color = color_red;
+console.log(first_color);
